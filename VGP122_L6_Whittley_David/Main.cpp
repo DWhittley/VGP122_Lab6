f3217305017e3f7d1aa368f460240a6b5e66606a
Tigris <matyasdudas97@gmail.com>
2022-11-27 11:16:58 -0800

    Update Main.cpp

--- VGP122_L6_Whittley_David/Main.cpp
+++ VGP122_L6_Whittley_David/Main.cpp
@@ -1,19 +1,10 @@
-/* Create a class called Rational for performing arithmetic with fractions. Write a program to test
-your class. Use integer variables to represent the private data of the class— the numerator and the
-denominator. Provide a constructor that enables an object of this class to be initialized when it’s
-declared. The constructor should contain default values in case no initializers are provided and
-should store the fraction in reduced form.
-
-For example, the fraction 2/4 would be stored in the object as 1 in the numerator and 2 in the denominator. 
-
-Provide public member functions that perform each of the following tasks:
-a. add—Adds two Rational numbers. The result should be stored in reduced form.
-b. subtract—Subtracts two Rational numbers. Store the result in reduced form.
-c. multiply—Multiplies two Rational numbers. Store the result in reduced form.
-d. divide—Divides two Rational numbers. The result should be stored in reduced form.
-e. toRationalString—Returns a string representation of a Rational number in the form a/b, where
-a is the numerator and b is the denominator.
-f. toDouble—Returns the Rational number as a double. */
+/* Create a RationalNumber(fractions) class like the one in Exercise 9.6.
+Provide the following capabilities:
+a. Create the class as a template class
+b. Create a constructor that prevents a 0 denominator in a fraction, reduces or simplifies
+fractions that are not in reduced form and avoids negative denominators.
+c. Overload the addition, subtraction, multiplication and division operators for this class.
+d. Overload the relational and equality operators for this class. */
 
 #include <iostream>
 #include "Rational.h"
